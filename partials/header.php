0410735999878a1aeb8cc1be718f6ea9e9542e1d
mishajib <mishajib222@gmail.com>
2020-09-06 00:22:28 +0600

    added new features

--- partials/header.php
+++ partials/header.php
@@ -2,6 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta property="og:image" content="assets/img/thumbnail.png">
+    <meta name="twitter:image" content="assets/img/thumbnail.png">
+    <meta name="twitter:site" content="ASCII CODE CONVERTER">
+    <meta property="og:site_name" content="ASCII CODE CONVERTER">
+    <meta name="description" content="By this app convert any character to ascii code and also convert ascii code to character">
+    <meta name="keywords" content="mishajib, mi shajib, mi, shajib, md mazharul islam shajib, mazharul islam, mazharul, ascii code finder, ascii code converter, character to ascii code, ascii code to character, ascii, ascii coder">
+    <meta name="author" content="MI SHAJIB">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASCII CODE FINDER</title>
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
